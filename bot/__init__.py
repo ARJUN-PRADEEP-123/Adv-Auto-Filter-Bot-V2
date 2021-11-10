db26394a2fe2544f5e3481b07592f63c46c9c2f1
ARJUN-PRADEEP-123 <93504468+ARJUN-PRADEEP-123@users.noreply.github.com>
2021-11-10 09:21:08 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -17,7 +17,7 @@ API_HASH = os.environ.get("API_HASH")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-DB_URI = os.environ.get("DB_URI")
+DB_URI = os.environ.get("DB_URI_THA_MWONE")
 
 USER_SESSION = os.environ.get("USER_SESSION")
 
