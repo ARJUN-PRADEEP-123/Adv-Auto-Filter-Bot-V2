6122d02f4d865f7089161899786410f1439d30aa
ARJUN-PRADEEP-123 <93504468+ARJUN-PRADEEP-123@users.noreply.github.com>
2021-11-11 09:48:20 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,21 +4,20 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey 11 {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>😀𝐇𝐄𝐘𝐘{}!!</b>
+<i>😉𝕀 𝔸𝕄 𝕎ℝ𝔸𝕐𝔸🙂
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+<i>😄I Am a Auto Filter Bot😊
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+<i>😎My Developer - Arjun Pradeep,(@Arjunbots)😎
 </i>"""    
     
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
+<b><i><u>🧐How To Use Me!?</u></i></b>
 
 <i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
+-> 😄Add Me To Any Group And Make Me Admin😋
+-> 😃Add Me To Your Desired Channel😉
 </i>
 
 <b>Bot Commands (Works Only In Groups) :</b>
@@ -43,16 +42,18 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@Arjunbots
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code>𝚆𝚁𝙰𝚈𝙰</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Arjunbots"</a></i></b>
+
+<b>➥ Developer</b> : <code>Arjun Pradeep</code>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Join Channel : <i><a href="https://t.me/squid_game_full_eps"</a></i>
 """
